474b47698a547d0d4a369b7d96da596e1603328d
aleja <aleja@LAPTOP-02I4B473>
2025-02-19 23:03:55 -0600

    Agregar archivos de proyecto.

---
+++ Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorresDeHanoi
+{
+    internal class Node
+    {
+        public Node Next;
+        public int Valor;
+        public Node(int Valor, Node Next)
+        {
+            this.Valor = Valor;
+            this.Next = Next;
+        }
+
+        public Node(int Valor)
+        {
+            this.Valor = Valor;
+            this.Next = null;
+        }
+
+        public Node()
+        {
+            this.Valor = 0;
+            this.Next = null;
+        }
+
+    }
+}
